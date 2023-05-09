be34140478ca214a6e84f152d52dbc40f96399ce
David Stinson <49245298+DavidStinson@users.noreply.github.com>
2023-05-08 20:54:45 -0500

    fix routes

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,12 +9,12 @@ const NavBar = ({ user, handleLogout }) => {
           <li>Welcome, {user.name}</li>
           <li><NavLink to="/profiles">Profiles</NavLink></li>
           <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
-          <li><NavLink to="/change-password">Change Password</NavLink></li>
+          <li><NavLink to="/auth/change-password">Change Password</NavLink></li>
         </ul>
       :
         <ul>
-          <li><NavLink to="/login">Log In</NavLink></li>
-          <li><NavLink to="/signup">Sign Up</NavLink></li>
+          <li><NavLink to="/auth/login">Log In</NavLink></li>
+          <li><NavLink to="/auth/signup">Sign Up</NavLink></li>
         </ul>
       }
     </nav>
