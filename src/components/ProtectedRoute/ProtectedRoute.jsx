f4b85cac6201e88ca068752832be268e1f515e00
David Stinson <49245298+DavidStinson@users.noreply.github.com>
2023-05-09 11:41:31 -0500

    fix protected route redirect

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -2,7 +2,7 @@
 import { Navigate } from 'react-router-dom'
 
 const ProtectedRoute = ({ user, children }) => {
-  if (!user) return <Navigate to="/login" />
+  if (!user) return <Navigate to="/auth/login" />
   return children
 }
 
